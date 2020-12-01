b052bf795669c2bd92618c655fdef7ccd0044ec8
nathan <nath.m.ormond@gmail.com>
2020-12-01 19:45:31 +0000

    potential build fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
+require("dotenv").config();
+
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken:
